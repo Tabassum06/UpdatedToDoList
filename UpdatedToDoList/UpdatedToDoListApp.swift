8274b4fd64d2a4ebf7b3b69c2ab107e7afc31f3d
Tabassum Zaman <tabassumzaman39@gmail.com>
2023-07-18 10:20:51 -0700

    Initial Commit

---
+++ UpdatedToDoList/UpdatedToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpdatedToDoListApp.swift
+//  UpdatedToDoList
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UpdatedToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
